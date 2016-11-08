3bcb7231f00ce1655fae52e10e627c39700d3919
matthschneider <matthias.schneider.11@hhu.de>
2016-11-08 15:07:46 +0100

    added mapping

--- plot_maps.R
+++ plot_maps.R
@@ -1,1 +1,28 @@
-### Different maps 
+### Different maps
+
+## Load packages
+
+library(data.table)
+
+## Install and load spatial packages
+
+install.packages("PBSmapping")
+library(PBSmapping)
+
+library(sp)
+library(rgdal)
+
+## Load the taxi data subset
+
+saveRDS(yellow, file="yellow.RDS")
+
+pickup <- data.frame(lon=yellow$pickup_longitude, 
+                     lat=yellow$pickup_latitude)
+pickup <- SpatialPoints(pickup)
+
+## Import the NYC shapefile
+load("nyc_spatial.RData")
+plot(nyc_spatial)
+
+points(pickup$lon, pickup$lat, col="red", cex=.4)
+plot(pickup, add=T, col="red")
