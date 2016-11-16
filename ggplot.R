9349a0fcfcff5ad49b1468db8b2d6d3658e5eea9
matthschneider <matthias.schneider.11@hhu.de>
2016-11-16 15:55:49 +0100

    added different maps

--- ggplot.R
+++ ggplot.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(sp)
 library(data.table)
+library(ggmap)
 
 yellow <- readRDS("yellow.RDS")
 load("nyc_spatial.RData")
@@ -19,14 +20,35 @@ yellow <- subset(yellow, pickup_longitude!=0 & pickup_longitude< -71 &
 
 ### Count each lat/long pair.
 
-yellow_count <- yellow[, list(paircount=.N), by=c("pickup_longitude", "pickup_latitude")]
+pickups <- yellow[, list(paircount=.N), by=c("pickup_longitude", "pickup_latitude")]
 
 
 yellow <- subset(yellow, pickup_latitude<41)
 yellow <- subset(yellow, pickup_latitude>-74.1)
+
+## Plot shapefile and each single pickup
 
 ggplot() + 
   geom_polygon(data=nyc_df, 
                aes(x=long, y=lat, group=group, colour=I("white"))) + 
   geom_point(data=yellow, size=.1, alpha=1/50, 
              aes(x=pickup_longitude, y=pickup_latitude, colour="yellow"))
+
+## Plot each pickup on the Manhattan Map
+
+m <- get_map(location="New York Manhattan", color="bw", zoom=12)
+p <- ggmap(m)
+p <- p + geom_point(data=pickups, size=.1, colour="yellow",
+                    aes(x=pickup_longitude, y=pickup_latitude)) + 
+     scale_alpha_continuous(range = c(0.14, 0.75), trans = "log", limits = range(pickups$paircount)) + 
+     scale_size_continuous(range = c(0.134, 0.173), trans = "log", limits = range(pickups$paircount)) + 
+     scale_color_manual(values = c("#ffffff", "#3f9e4d"))
+p
+
+## Heat map of Manhattan yellow taxi pickups
+
+m <- get_map(location="New York Manhattan", color="bw", zoom=12)
+p <- ggmap(m) + stat_density_2d(bins=30, geom='polygon', size=1, data=yellow, aes(x = pickup_longitude, y = pickup_latitude, alpha=..level.., fill = ..level..))
+p <- p  +  scale_fill_gradient(low = "yellow", high = "red", guide=FALSE) +  scale_alpha(range = c(0.02, 0.8), guide = FALSE) +xlab("") + ylab("")
+p
+
